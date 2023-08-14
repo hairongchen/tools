5a4b3febd4db8cdbcefc2285fd684d35cfcfe177
hairongchen <hairong.chen@intel.com>
2023-08-14 14:18:53 +0800

    remvoe rest trush

--- tdx/target/debug/build/libc-fa6d5edca43faa36/build_script_build-fa6d5edca43faa36.d
+++
@@ -1,5 +0,0 @@
-C:\Users\hairongc\OneDrive - Intel Corporation\Desktop\intel\mycode\tools\tdx\target\debug\build\libc-fa6d5edca43faa36\build_script_build-fa6d5edca43faa36.exe: C:\Users\hairongc\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.147\build.rs
-
-C:\Users\hairongc\OneDrive - Intel Corporation\Desktop\intel\mycode\tools\tdx\target\debug\build\libc-fa6d5edca43faa36\build_script_build-fa6d5edca43faa36.d: C:\Users\hairongc\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.147\build.rs
-
-C:\Users\hairongc\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.147\build.rs:
