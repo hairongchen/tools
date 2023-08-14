37a50aeb7bef54873567897e7849b1d90c4f0d1e
hairongchen <hairong.chen@intel.com>
2023-08-14 15:23:38 +0800

    format by rustfmt

--- tdx/src/main.rs
+++ tdx/src/main.rs
@@ -2,8 +2,8 @@ pub mod tee_tdx_lib;
 use tee_tdx_lib::*;
 
 fn main() {
-
-    let quote_vec = get_tdx_quote("1234567812345678123456781234567812345678123456781234567812345678".to_string());
-    println!("quote size = {}", quote_vec.len());
-
+    let q = get_tdx_quote(
+        "1234567812345678123456781234567812345678123456781234567812345678".to_string(),
+    );
+    println!("quote size = {}", q.len());
 }
